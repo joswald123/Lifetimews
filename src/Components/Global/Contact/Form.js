47abc38d10b1b63ea5b24e754bf69917da30b0c8
YeiiMacc <yeiimacc22@gmail.com>
2022-01-14 23:18:43 -0500

    css styles Contact form

--- src/Components/Global/Contact/Form.js
+++ src/Components/Global/Contact/Form.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
 
 import "../css/Form.css";
 
@@ -13,7 +12,7 @@ function Form() {
 
     })
 
-    let handleAdd = (event) =>{
+    let handleAdd = (event) => {
         const text = event.target.value;
         setFormData(text);
     };
@@ -24,68 +23,57 @@ function Form() {
     //     })
     // }
 
-    let handleSubmit = e =>{
+    let handleSubmit = e => {
         console.log("funcionando")
     }
+
     return (
-        <Container className='mt-5 mb-5'>
-            <Row>
-                <Col>
-                    <div className="contactForm">
-                        <p className="contactTitles">Contact Me</p>
-                    <div>
-                        <form action="/action_page.php">
-                        <label>First Name</label>
-                        <input
-                            onChange={handleAdd}
-                            value={formData.firstname}
-                            type="text" 
-                            id="fname" 
-                            name="firstname" 
-                            placeholder="Your name.." 
-                        />
-                        <label>Last Name</label>
-                        <input
-                            onChange={handleAdd}
-                            value={formData.lastname}
-                            type="text" 
-                            id="lname" 
-                            name="lastname" 
-                            placeholder="Your last name.." 
-                        />
+        <form action="/action_page.php">
+            <input
+                className="field"
+                onChange={handleAdd}
+                value={formData.firstname}
+                type="text"
+                id="fname"
+                name="firstname"
+                placeholder="Your name.."
+            />
 
+            <input
+                className="field"
+                onChange={handleAdd}
+                value={formData.lastname}
+                type="text"
+                id="lname"
+                name="lastname"
+                placeholder="Your last name.."
+            />
 
-                        <label>Email</label>
-                        <input
-                            onChange={handleAdd}
-                            value={formData.email}
-                            type="email" 
-                            id="email" 
-                            name="email" 
-                            placeholder="Your email" 
-                        />
+            <input
+                className="field"
+                onChange={handleAdd}
+                value={formData.email}
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Your email"
+            />
 
-
-                        <label>Message</label>
-                        <textarea
-                            onChange={handleAdd}
-                            value={formData.messsage}
-                            id="subject" 
-                            name="message" 
-                            placeholder="Write something.."
-                        ></textarea>
-                        <input
-                            onClick={handleSubmit}
-                            type="submit" 
-                            value="Submit" 
-                        />
-                        </form>
-                    </div>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
-    
+            <textarea
+                className="field"
+                onChange={handleAdd}
+                value={formData.messsage}
+                id="subject"
+                name="message"
+                placeholder="Write something.."
+            ></textarea>
+            <input
+            className="btnForm"
+                onClick={handleSubmit}
+                type="submit"
+                value="Submit"
+            />
+        </form>
     );
 }
 
--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Col, Container, FormGroup, Row } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
 
 // Components
 import Form from '../Global/Contact/Form'
@@ -7,6 +7,7 @@ import Map from '../Global/Contact/Map'
 
 // Images
 import backgroundImg from '../Global/images/officeAddressImg.PNG'
+import imgForm from '../Global/images/retirement_planning.png'
 
 //styles
 import "../Global/css/Contact.css"
@@ -19,37 +20,46 @@ function Contact() {
       <Container>
         <Row>
           <Col>
-              <h1>Contact</h1>
-              <span className="line"></span>
+            <h1>Contact</h1>
+            <span className="line"></span>
           </Col>
 
         </Row>
         <Row md={{ span: 6, offset: 3 }}>
           <Col>
-              <img src={backgroundImg} alt='backgroundImg' className='w-100 h-60'/>
+            <img src={backgroundImg} alt='backgroundImg' className='w-100 h-60' />
           </Col>
         </Row>
-        
+
         <Row className="mb-5 mt-2">
-          <Col xs={6}>
+          <Col className="rowMap">
             <Map />
           </Col>
-          <Col xs={6} >
+        </Row>
 
+
+        <Card className="mb-5">
+          <Row className="justify-content-center align-items-center">
+            <Col xs={12} md={6}>
               <p>4695 Chabot Drive, Suite 200 Pleasanton, CA 94588</p>
               <p>Phone: 925-447-0904</p>
               <p>germaine.cordes@lifetimecapital.com</p>
-
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Card className="mb-5">
-              <Form />
-            </Card> 
-          </Col>
-        </Row>
+            </Col>
+          </Row>
+        </Card>
       </Container>
+
+      <div className="contactBox">
+            <div className="contactForm mx-auto">
+              <div className="imgForm"></div>
+              <div className="formRight">
+                <h3 className="contactTitles">Contact Me</h3>
+                <Form />
+              </div>
+            </div>
+      </div>
+
+
     </div>
   );
 }
