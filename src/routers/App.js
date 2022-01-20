ea746d54f018fff178d265a209785e372fffbcf7
YeiiMacc <yeiimacc22@gmail.com>
2022-01-20 14:30:25 -0500

    Scroll to top with ReactJs

--- src/routers/App.js
+++ src/routers/App.js
@@ -1,5 +1,5 @@
 import './App';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 
 import Home from '../pages/Home';
 import About from '../pages/About';
@@ -10,28 +10,40 @@ import WhatWeDo from '../pages/WhatWeDo';
 import ErrorPage from '../pages/ErrorPage';
 import Service from '../pages/Service';
 import Layout from '../components/Layout';
+import { useLayoutEffect } from 'react';
 
 
 export default function App() {
+  // scroll to top menu
+  const Wrapper = ({ children }) => {
+    const location = useLocation();
+    useLayoutEffect(() => {
+      document.documentElement.scrollTo(0, 0);
+    }, [location.pathname]);
+    return children
+  }
+
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Layout />} >
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="businessRelationship" element={<BusinessRelationship />} />
-          <Route path="clientAccounts" element={<ClientAccounts />} />
+      <Wrapper>
 
-          <Route path="services" element={<WhatWeDo />} />
-          <Route path="services/:serviceId" element={<Service />} />
+        <Routes>
+          <Route path="/" element={<Layout />} >
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="businessRelationship" element={<BusinessRelationship />} />
+            <Route path="clientAccounts" element={<ClientAccounts />} />
 
-          <Route path="*" element={<ErrorPage />} />
-          {/* <Route path="*" element={ <Navigate replace to="/" /> }/> */}
-        </Route>
-      </Routes>
+            <Route path="services" element={<WhatWeDo />} />
+            <Route path="services/:serviceId" element={<Service />} />
 
+            <Route path="*" element={<ErrorPage />} />
+            {/* <Route path="*" element={ <Navigate replace to="/" /> }/> */}
+          </Route>
+        </Routes>
 
+      </Wrapper>
     </Router>
   );
 }
