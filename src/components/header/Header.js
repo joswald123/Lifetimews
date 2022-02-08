e5f1fa716b99f6c41de06aa167ede1200c6a40c5
YeiiMacc <yeiimaccdev@gmail.com>
2022-02-08 15:26:12 -0500

    logo menu style settings

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -28,8 +28,8 @@ export default function Header() {
             <img
               alt=""
               src={logo}
-              width="250"
-              height="60"
+              width="240"
+              height="50"
               className="d-inline-block align-top"
             />{" "}
           </Navbar.Brand>
