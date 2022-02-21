65260513d58564a8bd41ac513c0d6a34b2beeca3
YeiiMacc <yeiimaccdev@gmail.com>
2022-02-21 09:50:35 -0500

    adjust welcome text

--- src/components/home/DoubleCard.js
+++ src/components/home/DoubleCard.js
@@ -17,24 +17,22 @@ export default function DoubleCard() {
                     <img src={imgPhilosophy} alt="OUR PHILOSPHY" />
                     <h3 className='heading'>OUR PHILOSOPHY</h3>
                     <p>
-                        Our goal is to become your trusted financial advisor. Our commitment is to
-                        provide you with straight-forward information, helping you successfully pursue
-                        your financial goals and to simplify your financial life.
-                        Our practice has been built on the belief that long term relationships are more
-                        important than short term gains and that we will never sacrifice the former for
-                        the later.
+                        Our goal is to become your trusted financial advisor. Our commitment is to provide you with
+                        straight-forward information—helping you successfully pursue your financial goals and to
+                        simplify your financial life.
+                        Our practice has been built on the belief that long term relationships are more important than
+                        short term gains and that we will never sacrifice the former for the later.
                     </p>
                 </div>
 
                 <div className="box">
                     <img src={imgPhilosophy2} alt="VALUES BASES" />
-                    <h3  className='heading'>VALUES BASES</h3>
+                    <h3 className='heading'>VALUES BASES</h3>
                     <p>
-                        Throughout our process, we help you make decisions based on what is important
-                        to you, your life, and your financial goals. Your personal principles, values and
-                        beliefs influence every aspect of your life and are most satisfied when your
-                        financial and life goals meet your purpose; money matters only when it helps you
-                        live and build the life that matters the most to you.
+                        Throughout our process, we help you make decisions based on what is important to you, your
+                        life, and your financial goals. Your personal principles, values, and beliefs influence every aspect
+                        of your life and are most satisfied when your financial and life goals meet your purpose. Money
+                        matters only when it helps you build and live the life that matters the most to you.
                     </p>
                 </div>
             </div>
--- src/data/home/WelcomeText.js
+++ src/data/home/WelcomeText.js
@@ -1,32 +1,28 @@
 import React from 'react'
 
 export default function Welcome() {
-    
+
     return (
         <div>
             <p className='post__text'>
-                With over 30-years of experience in the financial services industry, Germaine
-                Cordes founded Lifetime Capital Financial Group to help her clients achieve
-                financial stability and peace of mind. Her client’s peace of mind comes from
-                knowing that their finances are properly coordinated and on the right track.
-                Simply put, we help our clients take full advantage of the opportunities available
-                to them and to make better financial decisions. Lifetime Capital leverages the
-                wealth management solutions available, with the focus on achieving, and then
-                maintaining, your financial independence.
+                With over 30-years of experience in the financial services industry, Germaine Cordes founded
+                Lifetime Capital Financial Group to help her clients achieve financial stability and peace of mind.
+                Her client’s peace of mind comes from knowing that their finances are properly coordinated
+                and on the right track. Simply put, Lifetime Capital helps our clients leverage the wealth
+                management solutions available with the focus on achieving and then maintaining your
+                financial independence.
             </p>
             <p className='post__text'>
-                We help our clients pursue their goals, by walking them through a step-by-step
-                process that provide clarity and solutions. As a “holistic” financial planner, care is
-                taken to optimize wealth creation, minimize taxes, and achieve tomorrow’s
-                retirement income needs. The goal, to live a better life without worrying about
-                money.
+                We help our clients pursue their goals, by walking them through a step-by-step process that
+                provide clarity and solutions. As a “holistic” financial planner, care is taken to optimize wealth
+                creation, minimize taxes, and achieve tomorrow’s retirement income needs. The goal, to live a
+                better life without worrying about money.
             </p>
             <p className='post__text'>
-                We are an independent firm that has formed strategic alliances with some of the
-                largest resources available in the financial services industry. These resources
-                provide some of the most innovative products and services available so that we
-                may customize financial plans and investment solutions to suit our client’s
-                needs.
+                We are an independent firm that has formed strategic alliances with some of the largest
+                resources available in the financial services industry. These resources provide some of the most
+                innovative products and services available so that we may customize financial plans and
+                investment solutions to suit our client&#39;s needs.
             </p>
         </div>
     )
