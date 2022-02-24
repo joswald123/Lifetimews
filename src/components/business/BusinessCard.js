d8da56cba30276d8293f368699d6d3bdd644bf35
joswald123 <48632836+joswald123@users.noreply.github.com>
2022-02-23 18:04:08 -0800

    Udated style for business cards

--- src/components/business/BusinessCard.js
+++ src/components/business/BusinessCard.js
@@ -1,53 +1,105 @@
-import React from 'react'
+import React from "react";
 
 // Data Content
-import AdvisorGroup from '../../data/Business/AdvisorGroup'
-import SecuritiesAmerica from '../../data/Business/SecuritiesAmerica'
-import AssetMark from '../../data/Business/AssetMark'
+import AdvisorGroup from "../../data/Business/AdvisorGroup";
+import SecuritiesAmerica from "../../data/Business/SecuritiesAmerica";
+import AssetMark from "../../data/Business/AssetMark";
 
 //  Img card
-import imageAdvisor from '../../images/Advisor.png'
-import imageSecurities from '../../images/Securities.png'
-import imageAssetMark from '../../images/AssetMark.png'
+import imageAdvisor from "../../images/Advisor.png";
+import imageSecurities from "../../images/Securities.png";
+import imageAssetMark from "../../images/AssetMark.png";
 
-import "./business.css"
+import "./business.css";
 
 export default function BusinessCard() {
-
     return (
         <div>
             <div>
-                <h1 className='title_h1 mt-3 pt-5'>Business Relationships</h1>
+                <h1 className="title_h1 mt-3 pt-5">Business Relationships</h1>
             </div>
-            <div className='container-cards'>
-                <div className='cbox'>
-                    <div className='cover'>
-                        <img src={imageAdvisor} width={250} height={150} alt="" />
-                        <div className='img_back_business'></div>
+
+            <div className="mainContainerCard">
+                <div className="theCard">
+                    <div className="theFront">
+                        <img
+                            src={imageAdvisor}
+                            width={250}
+                            height={150}
+                            alt="AdvisorGroup"
+                        />
+                        <div className="img_back_business"></div>
                     </div>
-                    <div className='box-body'>
+                    <div className="theBack1">
                         <AdvisorGroup />
                     </div>
                 </div>
-                <div className='cbox'>
-                    <div className='cover'>
-                        <img src={imageSecurities} width={250} height={150} alt="" />
-                        <div className='img_back_business'></div>
+
+                <div className="theCard">
+                    <div className="theFront">
+                        <img
+                            src={imageSecurities}
+                            width={250}
+                            height={150}
+                            alt="SecuritiesAmerica"
+                        />
+                        <div className="img_back_business"></div>
                     </div>
-                    <div className='box-body'>
+                    <div className="theBack2">
                         <SecuritiesAmerica />
                     </div>
                 </div>
-                <div className='cbox'>
-                    <div className='cover'>
-                        <img src={imageAssetMark} width={250} height={150} alt="" />
-                        <div className='img_back_business'></div>
+
+                <div className="theCard">
+                    <div className="theFront">
+                        <img
+                            src={imageAssetMark}
+                            width={250}
+                            height={150}
+                            alt="AssetMark"
+                        />
+                        <div className="img_back_business"></div>
                     </div>
-                    <div className='box-body'>
+                    <div className="theBack3">
                         <AssetMark />
                     </div>
                 </div>
             </div>
         </div>
-    )
+
+        // <div>
+        //     <div>
+        //         <h1 className='title_h1 mt-3 pt-5'>Business Relationships</h1>
+        //     </div>
+        //     <div className='container-cards'>
+        //         <div className='cbox'>
+        //             <div className='cover'>
+        //                 <img src={imageAdvisor} width={250} height={150} alt="" />
+        //                 <div className='img_back_business'></div>
+        //             </div>
+        //             <div className='box-body'>
+        //                 <AdvisorGroup />
+        //             </div>
+        //         </div>
+        //         <div className='cbox'>
+        //             <div className='cover'>
+        //                 <img src={imageSecurities} width={250} height={150} alt="" />
+        //                 <div className='img_back_business'></div>
+        //             </div>
+        //             <div className='box-body'>
+        //                 <SecuritiesAmerica />
+        //             </div>
+        //         </div>
+        //         <div className='cbox'>
+        //             <div className='cover'>
+        //                 <img src={imageAssetMark} width={250} height={150} alt="" />
+        //                 <div className='img_back_business'></div>
+        //             </div>
+        //             <div className='box-body'>
+        //                 <AssetMark />
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+    );
 }
