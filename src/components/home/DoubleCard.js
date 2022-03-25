954cbea81e407e7f4cc51221c97a980cc32f6a85
YeiiMacc <yeiimaccdev@gmail.com>
2022-03-24 22:18:59 -0500

    Philosophy settings on home page

--- src/components/home/DoubleCard.js
+++ src/components/home/DoubleCard.js
@@ -7,15 +7,13 @@ import imgPhilosophy2 from "../../images/philosophy2.png"
 
 export default function DoubleCard() {
     return (
-        <div className="philosophy mt-2" id="philosophy">
+        <div className="container-philosophy mt-2" id="philosophy">
 
-            {/* <h1 className="heading"> OUR PHILOSPHY </h1> */}
+            <section className="philosophyGrid">
 
-            <div className="box-container">
-
-                <div className="box">
+                <article className="box">
                     <img src={imgPhilosophy} alt="OUR PHILOSPHY" />
-                    <h3 className='heading'>OUR PHILOSOPHY</h3>
+                    <h3>OUR PHILOSOPHY</h3>
                     <p>
                         Our goal is to become your trusted financial advisor. Our commitment is to provide you with
                         straight-forward information—helping you successfully pursue your financial goals and to
@@ -23,19 +21,19 @@ export default function DoubleCard() {
                         Our practice has been built on the belief that long term relationships are more important than
                         short term gains and that we will never sacrifice the former for the later.
                     </p>
-                </div>
+                </article>
 
-                <div className="box">
+                <article className="box">
                     <img src={imgPhilosophy2} alt="VALUES BASES" />
-                    <h3 className='heading'>VALUES BASES</h3>
+                    <h3>VALUES BASES</h3>
                     <p>
                         Throughout our process, we help you make decisions based on what is important to you, your
                         life, and your financial goals. Your personal principles, values, and beliefs influence every aspect
                         of your life and are most satisfied when your financial and life goals meet your purpose. Money
                         matters only when it helps you build and live the life that matters the most to you.
                     </p>
-                </div>
-            </div>
+                </article>
+            </section>
         </div>
     )
 }
